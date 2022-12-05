effb15b56aeda1a16786d49725c6cd66f87905ca
philipe <philipe-leandro@hotmail.com>
2022-12-05 13:35:44 -0300

    Refactor controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def welcome
-    unless params[:search].blank?
-      summoner_name = params[:search]
-      response = Faraday.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/#{summoner_name.gsub(' ','%20')}?api_key=RGAPI-8ace8ea7-b33a-4a78-8fd3-f3a73b1049a4")
-      @response_body = JSON.parse(response.body)
-    end
+    @response_body = AccountLeagueOfLegends.return_info(params[:search])
   end
 end
