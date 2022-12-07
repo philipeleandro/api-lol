fd996d5ca0ca817282266e4a5bcb0e4001d9a040
philipe <philipe-leandro@hotmail.com>
2022-12-07 15:29:55 -0300

    Create request to get rank indo

--- app/services/riot_request.rb
+++ app/services/riot_request.rb
@@ -2,4 +2,8 @@ class RiotRequest
   def self.summoner_info(summoner_name)
     Faraday.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/#{summoner_name.gsub(' ','%20')}?api_key=#{ENV['API_LOL']}")
   end
+
+  def self.rank(id)
+    Faraday.get("https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/#{id}")
+  end
 end
