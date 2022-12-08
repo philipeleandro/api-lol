502971f1beb46a6d5a7f3ab9dcfcd986c9557c69
philipe <philipe-leandro@hotmail.com>
2022-12-08 09:46:36 -0300

    Create test to return_rank method

--- spec/services/account_league_of_legends_spec.rb
+++ spec/services/account_league_of_legends_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe AccountLeagueOfLegends do
   describe '.return_info' do
-    it 'returns string in json' do
+    it 'calls method to info account data' do
       allow(described_class).to receive(:return_info).with('Lyord').and_call_original
 
       described_class.return_info('Lyord')
 
       expect(described_class).to have_received(:return_info).with('Lyord')
+    end
+  end
+
+  describe '.return_rank' do
+    let(:encrypted_summonerid) { 'HMRc7Fg7VTIlcqzwKtRfGyaZ-LgadmmODx96lYHwzB7R7w' }
+
+    it 'calls method to info account rank' do
+      allow(described_class).to receive(:return_rank).with(encrypted_summonerid).and_call_original
+
+      described_class.return_rank(encrypted_summonerid)
+
+      expect(described_class).to have_received(:return_rank).with(encrypted_summonerid)
     end
   end
 end
