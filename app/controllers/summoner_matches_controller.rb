d565a53904b0bbd6ecff689af9aef51f39186570
philipe <philipe-leandro@hotmail.com>
2022-12-09 08:56:56 -0300

    Add alert message

--- app/controllers/summoner_matches_controller.rb
+++ app/controllers/summoner_matches_controller.rb
@@ -1,6 +1,13 @@
 class SummonerMatchesController < ApplicationController
   def index
     @account_info = AccountLeagueOfLegends.return_info(params['nickname'])
-    @rank = AccountLeagueOfLegends.return_rank(@account_info['id'])
+
+    if @account_info.nil?
+      flash.now[:alert] = 'Você deve voltar para o ínicio'
+      render 'index'
+    else
+      @account_info
+      @rank = AccountLeagueOfLegends.return_rank(@account_info['id'])
+    end
   end
 end
