91f36d0cada94a0ba90e2203b8343983f0ce1f77
philipe <philipe-leandro@hotmail.com>
2022-12-11 10:03:09 -0300

    Create new return of informations from riot

--- app/services/account_league_of_legends.rb
+++ app/services/account_league_of_legends.rb
@@ -10,4 +10,34 @@ class AccountLeagueOfLegends
       ConvertToJson.body_info(RiotRequest.rank(id).body)
     end
   end
+
+  def self.return_match_history(puuid)
+    unless puuid.blank?
+      ConvertToJson.body_info(RiotRequest.match_history(puuid).body)
+    end
+  end
+
+  def self.return_match_info(match_id)
+    unless match_id.blank?
+      ConvertToJson.body_info(RiotRequest.match_info(match_id).body)['metadata']['participants']
+    end
+  end
+
+  def self.return_summoner_name_by_puuid(puuid)
+    unless puuid.blank?
+      ConvertToJson.body_info(RiotRequest.summoner_info_by_puuid(puuid).body)['name']
+    end
+  end
+
+  def self.search_for_player_from_history(puuid)
+    unless puuid.blank?
+      id_matches = return_match_history(puuid)
+
+      id_matches.map { |match_id| return_match_info(match_id) }.map do |summoners|
+        summoners.map do |summoner|
+          return_summoner_name_by_puuid(summoner) 
+        end
+      end
+    end
+  end
 end
--- spec/services/account_league_of_legends_spec.rb
+++ spec/services/account_league_of_legends_spec.rb
@@ -22,4 +22,41 @@ describe AccountLeagueOfLegends do
       expect(described_class).to have_received(:return_rank).with(encrypted_summonerid)
     end
   end
+
+  describe '.return_match_history' do
+    let(:puuid) { 'v2jpHm5Vm2EhGiQT_TwtHxiiQSc7sa_WiHb6nXjFaqXtqDx0ZObPsFiI_0k3qd5wCFRmEi9rUvMbeg' }
+
+    it 'returs array with match id' do
+      allow(described_class).to receive(:return_match_history).with(puuid).and_return(['BR_TESTHISTORY'])
+
+      response = described_class.return_match_history(puuid)
+
+      expect(response).to eq ['BR_TESTHISTORY']
+    end
+  end
+
+  describe '.return_summoner_name_by_puuid' do
+    let(:puuid) { 'v2jpHm5Vm2EhGiQT_TwtHxiiQSc7sa_WiHb6nXjFaqXtqDx0ZObPsFiI_0k3qd5wCFRmEi9rUvMbeg' }
+
+    it 'returns summuner name' do
+      allow(described_class).to receive(:return_summoner_name_by_puuid).with(puuid).and_call_original
+
+      response = described_class.return_summoner_name_by_puuid(puuid)
+
+      expect(response).to eq 'BlackHoot'
+    end
+  end
+
+  describe '.search_for_player_from_history' do
+    let(:match_id) { 'BR1_2404338530' }
+
+    it 'returns array with summoner names from match ' do
+      players_nicknames = ['summoner','summoner','summoner','summoner','summoner','summoner','summoner','summoner','summoner','summoner']
+      allow(described_class).to receive(:search_for_player_from_history).with(match_id).and_return(players_nicknames)
+
+      response = described_class.search_for_player_from_history(match_id)
+
+      expect(response).to eq players_nicknames
+    end
+  end
 end
