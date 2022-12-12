565a519662d2b4c4001bd618a55a647b93543be9
philipe <philipe-leandro@hotmail.com>
2022-12-12 19:56:12 -0300

    Refactor tests

--- spec/services/account_league_of_legends_spec.rb
+++ spec/services/account_league_of_legends_spec.rb
@@ -5,21 +5,24 @@ describe AccountLeagueOfLegends do
     it 'calls method to info account data' do
       allow(described_class).to receive(:return_info).with('Lyord').and_call_original
 
-      described_class.return_info('Lyord')
-
-      expect(described_class).to have_received(:return_info).with('Lyord')
+      result = described_class.return_info('Lyord')
+      
+      expect(result.keys).to include 'id'
+      expect(result.keys).to include 'name'
+      expect(result.keys).to include 'summonerLevel'
     end
   end
 
   describe '.return_rank' do
-    let(:encrypted_summonerid) { 'HMRc7Fg7VTIlcqzwKtRfGyaZ-LgadmmODx96lYHwzB7R7w' }
+    let(:encrypted_summonerid) { 'AsH5zLUcK6wNDoIzDe_3dhS-y_462N7K0wnK64-i4NMwGA' }
 
     it 'calls method to info account rank' do
       allow(described_class).to receive(:return_rank).with(encrypted_summonerid).and_call_original
 
-      described_class.return_rank(encrypted_summonerid)
+      result = described_class.return_rank(encrypted_summonerid)
 
-      expect(described_class).to have_received(:return_rank).with(encrypted_summonerid)
+      expect(result.first.keys).to include 'rank'
+      expect(result.first.keys).to include 'leaguePoints'
     end
   end
 
