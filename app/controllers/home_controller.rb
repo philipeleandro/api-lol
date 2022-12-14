e322614f3de66b30b2793ede94b5bd2f402ff357
philipe <philipe-leandro@hotmail.com>
2022-12-14 10:01:44 -0300

    Change variable position to avoid nil error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   def welcome
     if params[:search].present?
       @account_info = AccountLeagueOfLegends.return_info(params[:search])
-      @encrypt_summoner_info = EncryptAndDecrypt.encrypt_summoner_info(@account_info)
-
+      
       if @account_info['id'].present?
+        @encrypt_summoner_info = EncryptAndDecrypt.encrypt_summoner_info(@account_info)
         redirect_to controller: 'summoner_matches', action: 'index', params: { nickname: params[:search], summoner_info: @encrypt_summoner_info }
       else
         flash.now[:alert] = 'Invocador não encontrado!'
--- app/controllers/summoner_matches_controller.rb
+++ app/controllers/summoner_matches_controller.rb
@@ -1,11 +1,10 @@
 class SummonerMatchesController < ApplicationController
   def index
-    @account_info = EncryptAndDecrypt.decrypt_summoner_info(params[:summoner_info])
-    
-    if @account_info.nil?
+    if params[:summoner_info].nil?
       flash.now[:alert] = 'Você deve voltar para o ínicio'
       render 'index'
     else
+      @account_info = EncryptAndDecrypt.decrypt_summoner_info(params[:summoner_info])
       @rank = AccountLeagueOfLegends.return_rank(@account_info['id'])
       @history_and_players = AccountLeagueOfLegends.search_for_player_from_history(@account_info['puuid'])
     end
