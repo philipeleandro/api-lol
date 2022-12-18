b40d45ae17120bcdbd50477cddb579dc66a076e3
philipe <philipe-leandro@hotmail.com>
2022-12-18 10:04:05 -0300

    Add configuration for capybara-screenshot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require_relative'./support/vcr_setup'
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
