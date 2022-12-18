bda9e18d3df40ce87f1334b3971d0071ebaf4257
philipe <philipe-leandro@hotmail.com>
2022-12-18 10:04:20 -0300

    Add system test for show action

---
+++ spec/system/summoner_matches/person_view_summoner_profile_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User search a profile' do
+  context 'when view summoner profile in match' do
+    it 'and finds result' do
+      VCR.use_cassette('system/summoner_match/user_view_a_summoner_profile_in_history_success') do
+        visit root_path
+        fill_in 'search', with: 'Lyord'
+        click_on 'Search'
+        VCR.use_cassette('system/summoner_match/user_click_a_summoner_profile_in_history_successs') do
+          first(:link, 'Lyord').click
+
+          expect(page).to have_content 'Lyord'
+          expect(page).to have_content 'Soloq/Duo Rank'
+          expect(page).to have_content 'Flex Rank'
+          expect(page).to have_content 'Histórico de Partida'
+          expect(current_path).to eq summoner_matches_path
+        end
+      end
+    end
+  end
+end
