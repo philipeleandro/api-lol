30de8dd44f938ee0164bb4ee0b8f93027a2eccb2
philipe <philipe-leandro@hotmail.com>
2022-12-23 08:43:31 -0300

    Create callback to encode nickname

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+  before_action :set_encode_nick, only: [:welcome]
+
   def welcome
     if params[:search].present?
+      params[:search] = params[:search].join('')
+
       @account_info = AccountLeagueOfLegends.return_info(params[:search])
       
       if @account_info['id'].present?
@@ -9,6 +13,20 @@ class HomeController < ApplicationController
       else
         flash.now[:alert] = 'Invocador não encontrado!'
         render 'welcome'
+      end
+    end
+  end
+
+  private
+
+  def set_encode_nick
+    if params[:search].present? 
+      params[:search] = params[:search].force_encoding('iso-8859-1').split('')
+
+       params[:search].each_with_index do |char,index|
+        if char.match?(/\W/)
+           params[:search][index] = '%' + "%02X" % [ char.ord ]
+        end
       end
     end
   end
