a9b1bb6bf47ffd535cd353e8402df96fb1013bf6
philipe <philipe-leandro@hotmail.com>
2022-12-25 09:35:15 -0300

    Add method to encode nickname in uri

--- app/services/riot_request.rb
+++ app/services/riot_request.rb
@@ -1,6 +1,6 @@
 class RiotRequest
   def self.summoner_info(summoner_name)
-    Faraday.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/#{summoner_name.gsub(' ','%20')}?api_key=#{ENV['API_LOL']}")
+    Faraday.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/#{ERB::Util.url_encode(summoner_name)}?api_key=#{ENV['API_LOL']}")
   end
 
   def self.rank(id)
@@ -19,4 +19,3 @@ class RiotRequest
     Faraday.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/#{puuid}?api_key=#{ENV['API_LOL']}")
   end
 end
-
