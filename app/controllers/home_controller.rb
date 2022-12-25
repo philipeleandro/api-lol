db2e896306430b108613562d3bcb6a214335178c
philipe <philipe-leandro@hotmail.com>
2022-12-25 09:34:37 -0300

    Remove useless call back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-  before_action :set_encode_nick, only: [:welcome]
-
   def welcome
     if params[:search].present?
       @account_info = AccountLeagueOfLegends.return_info(params[:search])
@@ -12,22 +10,6 @@ class HomeController < ApplicationController
         flash.now[:alert] = 'Invocador não encontrado!'
         render 'welcome'
       end
-    end
-  end
-
-  private
-
-  def set_encode_nick
-    if params[:search].present? 
-      params[:search] = params[:search].force_encoding('iso-8859-1').split('')
-
-       params[:search].each_with_index do |char,index|
-        if char.match?(/\W/)
-           params[:search][index] = '%' + "%02X" % [ char.ord ]
-        end
-      end
-      
-      params[:search] = params[:search].join('')
     end
   end
 end
