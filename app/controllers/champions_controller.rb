17ccf8e2b4ec20390ff20e9dc0ab93d03fcb836f
philipe <philipe-leandro@hotmail.com>
2023-01-01 10:08:07 -0300

    Create index for champions

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,4 +1,5 @@
 class ChampionsController < ApplicationController
   def index
+    @champions = Champion.all
   end
 end
--- spec/system/champions/user_accesses_champions_page_spec.rb
+++ spec/system/champions/user_accesses_champions_page_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'User accesses champions page' do
-  it 'and find champ infos' do
+  it 'and find champions list' do
     visit root_path
     click_on 'Campeões'
 
     expect(current_path).to eq champions_path
-    expect(page).to have_content 'Champions'
+    expect(page).to have_content 'Lista de Campeões'
   end
 end
