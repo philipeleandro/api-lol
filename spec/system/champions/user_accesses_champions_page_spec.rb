167a3e2489d6fb08035035817808d27c3c62a325
philipe <philipe-leandro@hotmail.com>
2023-01-03 10:41:33 -0300

    Refactor test

--- spec/system/champions/user_accesses_champions_page_spec.rb
+++ spec/system/champions/user_accesses_champions_page_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 describe 'User accesses champions page' do
+  let!(:champion) { create(:champion) }
+
+  before do
+    create_list(:champion, 4)
+  end
+
   it 'and find champions list' do
     visit root_path
     click_on 'Campeões'
-
+    
     expect(current_path).to eq champions_path
     expect(page).to have_content 'Lista de Campeões'
+    expect(page).to have_content champion.name
   end
 end
