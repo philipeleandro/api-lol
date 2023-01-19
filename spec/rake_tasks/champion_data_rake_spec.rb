dd0920418365deb9438a58d23418e13184a15049
philipe <philipe-leandro@hotmail.com>
2023-01-18 22:10:42 -0300

    Refactor rake test

--- spec/rake_tasks/champion_data_rake_spec.rb
+++ spec/rake_tasks/champion_data_rake_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
-describe '' do
-  context 'when there is no champion' do
+describe 'champion_data:populate' do
+  before do
+    Rails.application.load_tasks
+  end
+
+  after(:each) do 
+    Rake::Task.clear
+  end
+  
+  context 'when there is no champion in database' do
     it 'creates champions in db' do
       VCR.use_cassette('services/tasks/champion_data:populate') do
-        Rails.application.load_tasks
-
         Rake::Task['champion_data:populate'].invoke
 
         expect(Champion.first.name).to eq 'Aatrox'
@@ -15,13 +21,12 @@ describe '' do
 
   context 'when there is champion' do
     let!(:champion) { create(:champion, name: 'Aatrox', key: 'Ali')}
-
+  
     it 'creates champions in db' do
       VCR.use_cassette('services/tasks/champion_data:populate') do
-        Rails.application.load_tasks
-
+  
         Rake::Task['champion_data:populate'].invoke
-
+        
         expect(Champion.first.name).to eq 'Aatrox'
         expect(Champion.first.key).to eq '266'
       end
